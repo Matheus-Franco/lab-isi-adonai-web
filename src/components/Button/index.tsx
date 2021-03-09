ef5a5aa31b20323b65d097ea8be75b102df66a3e
Matheus <matheusfrancoalves@hotmail.com>
2021-03-09 00:25:46 -0300

    :bug: format code selection

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,7 +3,7 @@ import * as S from './styles';
 
 interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     loading?: boolean;
-  };
+};
 
 const Button: React.FC<IButtonProps> = ({ children, loading, ...props }) => {
   const renderChildren = loading ? 'Carregando...' : children
