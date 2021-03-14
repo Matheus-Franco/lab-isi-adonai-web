70104f0e9d981813d4b0c2f6cf15eb1404f7c68d
Matheus <matheusfrancoalves@hotmail.com>
2021-03-14 17:48:28 -0300

    :sparkles: add keyframes style

--- src/pages/employees/Dashboard/styles.ts
+++ src/pages/employees/Dashboard/styles.ts
@@ -1,4 +1,14 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const appearFromRight = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(-20px)
+  } to {
+    opacity: 1;
+    transform: translateY(0)
+  }
+`
 
 export const Container = styled.div`
     height: 100%;
@@ -15,10 +25,12 @@ export const FilterItems = styled.div`
     align-items: center;
     display: flex;
     margin: 24px 0;
+    animation: ${appearFromRight} 1s;
 `
 
 export const AvailableItemsContainer = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr 1fr 1fr;
     gap: 24px;
+    margin: 24px 0;
 `
