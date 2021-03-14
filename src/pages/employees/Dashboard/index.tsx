cda7e8d9491817ddbbc84f0e45be20ff8d4244b4
Matheus <matheusfrancoalves@hotmail.com>
2021-03-14 17:08:58 -0300

    :sparkles: create filterItems area

--- src/pages/employees/Dashboard/index.tsx
+++ src/pages/employees/Dashboard/index.tsx
@@ -1,20 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './styles';
 
 import Header from '../../../components/Header';
+import Button from '../../../components/Button';
 
 const Dashboard: React.FC = () => {
+  const [filterItemsIsOpen, setFilterItemsIsOpen] = useState<boolean>(false)
+
+  const toggleOpenFilterItems = () => setFilterItemsIsOpen(!filterItemsIsOpen)
+
+  const renderFilterItems = filterItemsIsOpen && (
+    <S.FilterItems>
+      <select>
+        <option>Preço</option>
+      </select>
+      <select>
+        <option>Ano</option>
+      </select>
+      <select>
+        <option>Tipo de Operação</option>
+      </select>
+
+      <Button>Aplicar</Button>
+    </S.FilterItems>
+  )
+
   return (
     <>
       <Header />
       <S.Container>
-          <S.FilterSidebar>
-            filter menu
-          </S.FilterSidebar>
+        <S.Title>
+          <h1>Nossos veículos disponíveis</h1>
+          <Button onClick={toggleOpenFilterItems}>
+            Filtros
+          </Button>
+        </S.Title>
 
-          <S.Content>
-            main content
-          </S.Content>
+        {renderFilterItems}
+
+        <S.AvailableItemsContainer>
+          <div>
+            olá
+          </div>
+
+          <div>
+            olá
+          </div>
+
+          <div>
+            olá
+          </div>
+
+          <div>
+            olá
+          </div>
+        </S.AvailableItemsContainer>
       </S.Container>
     </>
   )
--- src/pages/employees/Dashboard/styles.ts
+++ src/pages/employees/Dashboard/styles.ts
@@ -2,15 +2,39 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     height: 100%;
-
-    display: flex;
-`;
-
-export const FilterSidebar = styled.div`
-    width: 200px;
-    max-width: 200px;
-    background: blue;
-    height: 100vh;
+    padding: 48px 200px;
 `
 
-export const Content = styled.div``
+export const Title = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+export const FilterItems = styled.div`
+    align-items: center;
+    display: flex;
+    margin: 24px 0;
+
+    select {
+        padding: 16px;
+        margin-right: 24px;
+        border-radius: 4px;
+        background: #fff;
+        border-color: #ff9000;
+        color: #ff9000;
+    }
+`
+
+export const AvailableItemsContainer = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    gap: 24px;
+
+    div {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background: blue;
+    }
+`
