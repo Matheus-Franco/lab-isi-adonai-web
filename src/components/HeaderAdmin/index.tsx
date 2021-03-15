8c0c78a931f5a322c8d034b2e901dece7f7f132a
Matheus <matheusfrancoalves@hotmail.com>
2021-03-15 19:53:44 -0300

    :sparkles: navigate from new-offer admin to login

--- src/components/HeaderAdmin/index.tsx
+++ src/components/HeaderAdmin/index.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import * as S from './styles';
 
 import { VscSignOut } from 'react-icons/vsc';
+import { useHistory } from 'react-router';
 
 const HeaderAdmin: React.FC = () => {
+    const { push } = useHistory();
+
+    const handleNavigate = () => push('/admin');
+
     return (
         <S.Container>
             <div>
@@ -18,7 +23,7 @@ const HeaderAdmin: React.FC = () => {
             </div>
 
             <S.UserActions>
-                <VscSignOut size={24} />
+                <VscSignOut size={24} onClick={handleNavigate} />
             </S.UserActions>
         </S.Container>
     )
