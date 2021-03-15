7acdab1eef600ac07591806178c9cb1548818d1c
Matheus <matheusfrancoalves@hotmail.com>
2021-03-15 20:09:07 -0300

    :sparkles: receive productId prop in ProductItem

--- src/components/ProductItem/index.tsx
+++ src/components/ProductItem/index.tsx
@@ -2,17 +2,24 @@
 import React, { ImgHTMLAttributes } from 'react';
 import * as S from './styles';
 
+import { useHistory } from 'react-router';
+
 interface IProductItemProps extends ImgHTMLAttributes<HTMLImageElement> {
     productName: string
+    productId?: string
 }
 
-const ProductItem: React.FC<IProductItemProps> = ({ productName, ...props }) => {
+const ProductItem: React.FC<IProductItemProps> = ({ productName, productId, ...props }) => {
+    const { push } = useHistory();
+    
+    const handleNavigate = () => push('/product-details');
+
     return (
         <S.Container>
                 <img {...props} />
                 <div>
                     <p>{productName}</p>
-                    <a href="#">Ver detalhes</a>
+                    <a href="#" onClick={handleNavigate}>Ver detalhes</a>
                 </div>
         </S.Container>
     )
