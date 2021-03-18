2febde5d53ab10296283e47546102427569169e1
Matheus <matheusfrancoalves@hotmail.com>
2021-03-18 18:31:34 -0300

    :sparkles: authenticate client

--- src/pages/clients/Login/index.tsx
+++ src/pages/clients/Login/index.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './styles';
 
 import Input from '../../../components/Input';
 import Button from '../../../components/Button';
 
 import { useHistory } from 'react-router';
+import { useAuth } from '../../../hooks/clientAuth';
 
 const Login: React.FC = () => {
   const { push } = useHistory();
+  const { signIn } = useAuth();
 
-  const handleNavigate = () => push('/dashboard');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleLogin = async () => {
+    try {
+      await signIn({
+        email,
+        password
+      });
+
+      push('/dashboard')
+    } catch (e) {
+      console.log('Erro')
+    }
+  }
 
   return (
       <S.Container>
@@ -20,9 +36,18 @@ const Login: React.FC = () => {
             <p>Faça login com a sua conta</p>
 
             <S.Form>
-                <Input placeholder="E-mail" />
-                <Input placeholder="Senha" type="password" />
-                <Button onClick={handleNavigate}>Entrar</Button>
+                <Input 
+                  placeholder="E-mail"  
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input 
+                  placeholder="Senha" 
+                  type="password" 
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <Button onClick={handleLogin}>Entrar</Button>
             </S.Form>
           </S.Content>
       </S.Container>
